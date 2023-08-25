6b8e43355f6ef7a4e98b5ae34b2e2e56b8671d9a
Biraj <birajkmc4@gmail.com>
2023-08-25 15:08:11 +0545

    POST request to firebase

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,7 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import NewMeetupForm from "../components/Meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-    return(
-        <div>New Meetup Page</div>
-    )
+  const navigate = useNavigate();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-meeting-48baa-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: { "Content-type": "application/json" },
+      }
+    ).then(() => {
+      navigate("/");
+    });
+  }
+
+  return (
+    <section>
+      <div>
+        <h1>Add New Meetup</h1>
+      </div>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
