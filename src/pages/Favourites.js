4021d549e0d27d22d3f728181b4b98a61a3e547a
Biraj <birajkmc4@gmail.com>
2023-08-27 15:39:13 +0545

    Final Commit

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,7 +1,23 @@
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from '../components/Meetups/MeetupList'
+
 function FavouritesPage() {
-    return(
-        <div>Favourites Page</div>
-    )
+  const favoritesCtx = useContext(FavoritesContext);
+
+  let content;
+  if (favoritesCtx.totalFavorites === 0){
+    content = <p>You get no favorites yet. Start adding some?</p>
+  }else{
+    content = <MeetupList meetups={favoritesCtx.favorites} />
+  }
+
+  return (
+    <section>
+      <h1>My Favourites</h1>
+      {content}
+    </section>
+  );
 }
 
 export default FavouritesPage;
